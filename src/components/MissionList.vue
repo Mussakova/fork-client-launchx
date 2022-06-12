30e255ed02cad56cf461f9dbe67892e4e7b3b759
Hirepan E <ginebrasyrel@gmail.com>
2022-06-11 21:24:48 -0500

    Agregando template de missioncommander

---
+++ src/components/MissionList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="list row">
+        <h4> Mission Comander</h4>z
+        <ul class="list-group">
+           <li class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(mission, index) in missions"
+          :key="index"
+          
+        >
+          {{ mission.name }}
+        </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import MissionService from "../services/MissionService";
+
+export default{
+    name: "missionCommander-list",
+     data() {
+    return {
+   
+    mission: [],
+    currentMission: null
+     
+    };
+  },
+    methods: {
+    getAllMission(){
+         MissionService.getAllMission()
+        .then(response => {
+          this.mission = response.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+} ,
+
+mounted() {
+    this.getAllMission();
+  }
+};
+</script>
